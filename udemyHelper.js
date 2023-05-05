9cbe85c84c66e22f7708cac3029e52127f410c49
TakayukiHiguchi <higutaka7183dev@gmail.com>
2023-05-05 09:08:55 +0900

    非同期処理。他ファイルからの参照。を追加

---
+++ udemyHelper.js
@@ -0,0 +1,4 @@
+const name="Mike"
+
+//定数を他ファイルで使えるようにする
+module.exports=name;
--- udemySample.js
+++ udemySample.js
@@ -1,1 +1,28 @@
-console.log("My first app!")
+//処理の書き方
+console.log("1コンソール出力")
+
+//関数使い方
+function test(){
+    console.log("2関数でのコンソール出力")
+}
+test();
+
+//定数を他ファイルから持ってくる
+//「./」で同じ階層を指定
+const name=require("./udemyHelper");
+
+console.log(name)
+
+//非同期処理
+setTimeout(function(){
+    console.log("3タイマー関数での出力")
+}, 5000)
+
+setTimeout(function(){
+    console.log("3タイマー関数での出力2個目")
+}, 5000)
+
+setTimeout(function(){
+    console.log("3タイマー関数での出力3個目")
+    console.log("5秒待ちは後ろの非同期処理→関数呼び出し自体はほぼ同時")
+}, 5000)
