7e2b59222cfff48ea4a1cc2091d1583f5cba40f7
TakayukiHiguchi <higutaka7183dev@gmail.com>
2023-05-06 13:05:57 +0900

    Aboutページでhtml返却を検証

--- web-server/index.js
+++ web-server/index.js
@@ -4,13 +4,13 @@ const app = express()
 //イベントドリブン（引数が来る→中の処理出し分け）
 app.get('/', function (req, res) {
     console.log("Topページの出力");
-    console.log(req);
+    //console.log(req);
     res.send('Topページです')
 })
 
 app.get('/About', function (req, res) {
     console.log("Aboutページの出力");
-    res.send('Aboutページです')
+    res.send('<h1>Aboutページです!</h1><h2>htmlを返却することもできます</h2>')
 })
 
 app.get('/api/v1/users', function (req, res) {
