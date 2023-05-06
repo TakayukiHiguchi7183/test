ff19d9165fe1b0596d617afb95e3ee27c48e944b
TakayukiHiguchi <higutaka7183dev@gmail.com>
2023-05-06 15:17:04 +0900

    フロントエンドをpublicに分割

--- web-server/index.js
+++ web-server/index.js
@@ -1,17 +1,26 @@
 const express = require('express')
+const path=require("path");
 const app = express()
 
-//イベントドリブン（引数が来る→中の処理出し分け）
-app.get('/', function (req, res) {
-    console.log("Topページの出力");
-    //console.log(req);
-    res.send('Topページです')
-})
+//requestのbodyの値を使えるようにする
+app.use(express.urlencoded({extended: false}))
+//public配下をフロントエンドに設定する
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/About', function (req, res) {
-    console.log("Aboutページの出力");
-    res.send('<h1>Aboutページです!</h1><h2>htmlを返却することもできます</h2>')
-})
+app.get('/manage', function (req, res) {
+    console.log("管理者ページの出力");
+    res.send('<h1>管理用ページです!</h1><h3>直書きでhtmlを返却することもできます</h3>')
+});
+
+app.post('/api/v1/quiz', function (req, res) {
+    const answer=req.body.answer;
+    if(answer==="2"){
+        //publicの中の相対パス
+        res.redirect("/correct.html");
+    }else{
+        res.send("<h1>不正解、、</h1>");
+    }
+});
 
 app.get('/api/v1/users', function (req, res) {
     console.log("APIでユーザ情報取得");
@@ -19,7 +28,7 @@ app.get('/api/v1/users', function (req, res) {
         name: "Mike",
         age: 30
     })
-})
+});
 
 app.listen(3000, function(){
     console.log("I am running!");
