118479ae78a1ec456670a04e611bc396e7e060e7
TakayukiHiguchi <higutaka7183dev@gmail.com>
2023-05-06 12:44:57 +0900

    Node.jsでwebサーバ立ち上げ

---
+++ web-server/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+
+//イベントドリブン（引数が来る→中の処理出し分け）
+app.get('/', function (req, res) {
+    console.log("Topページの出力");
+    console.log(req);
+    res.send('Topページです')
+})
+
+app.get('/About', function (req, res) {
+    console.log("Aboutページの出力");
+    res.send('Aboutページです')
+})
+
+app.get('/api/v1/users', function (req, res) {
+    console.log("APIでユーザ情報取得");
+    res.send({
+        name: "Mike",
+        age: 30
+    })
+})
+
+app.listen(3000, function(){
+    console.log("I am running!");
+});
+
+console.log("最終行です");
